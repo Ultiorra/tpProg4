db8c8aeb733814a86bb2cba77c1199b13f22ae0a
Ultiorra <jarreaumik@hotmail.com>
2024-01-08 11:37:18 +0100

    2.12 done

--- r05.05-tp-corrige-fin-tp3/src/app/mon-compte/layout.tsx
+++ r05.05-tp-corrige-fin-tp3/src/app/mon-compte/layout.tsx
@@ -9,14 +9,19 @@ import { cookies } from "../../../node_modules/next/headers";
 import { getUser } from "../../utils/supabase";
 import {redirect} from "next/navigation";
 export default async function Layout({ children }: { children: ReactNode }) {
-  const orders = await prisma.order.findMany();
+
   const supabase  = createServerComponentClient({cookies})
   const user = await getUser(supabase)
-
-
     if (!user || !user.session ) {
         redirect('/connexion')
     }
+    const orders = await prisma.order.findMany({
+        where: {
+            userId: user.session.user.id
+        }
+    });
+
+
   return (
     <>
       {/* Orders list */}
