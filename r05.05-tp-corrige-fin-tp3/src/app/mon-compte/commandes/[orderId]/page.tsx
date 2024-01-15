1344632c681f19d9899765c77491b270558d2a44
Ultiorra <jarreaumik@hotmail.com>
2024-01-15 14:34:24 +0100

    3.2

--- r05.05-tp-corrige-fin-tp3/src/app/mon-compte/commandes/[orderId]/page.tsx
+++ r05.05-tp-corrige-fin-tp3/src/app/mon-compte/commandes/[orderId]/page.tsx
@@ -2,12 +2,30 @@ import { OrderDetailsLayout } from "tp-kit/components";
 import { NextPageProps } from "../../../../types";
 import prisma from "../../../../utils/prisma";
 import { notFound } from "next/navigation";
+import {createServerComponentClient} from "@supabase/auth-helpers-nextjs";
+import {cookies} from "next/headers";
+import RealTimeOrderDetails from "../../../../components/RealTimeOrderDetails";
 
 type Props = {
   orderId: string;
 }
 
 export default async function OrderDetailsPage({params}: NextPageProps<Props>) {
+  const supabase = createServerComponentClient({cookies})
+  const channel = supabase
+      .channel('schema-db-changes')
+      .on(
+          'postgres_changes',
+          {
+            event: 'UPDATE',
+            schema: 'public',
+          },
+          (payload) => {
+            console.log('Received event on channel schema-db-changes', payload);
+
+          }
+      )
+      .subscribe()
   const orderId = parseInt(params.orderId);
   const order = await prisma.order.findUnique({
     where: {id: orderId},
@@ -20,5 +38,5 @@ export default async function OrderDetailsPage({params}: NextPageProps<Props>) {
 
   if (!order) notFound();
 
-  return <OrderDetailsLayout order={order} />
+  return <RealTimeOrderDetails order={order} />;
 }
---
+++ r05.05-tp-corrige-fin-tp3/src/components/RealTimeOrderDetails.tsx
@@ -0,0 +1,47 @@
+"use client"
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useEffect, useState } from "react";
+import { OrderDetailsLayout } from "tp-kit/components";
+import { OrderData } from "tp-kit/types";
+
+
+type Props = {
+    order : any
+}
+
+export default function RealTimeOrderDetails({order} : Props) {
+
+    const supabase = createClientComponentClient()
+    const [current, setCurrent] = useState(order);
+
+    useEffect(() => {
+        const channel = supabase.channel("supabase_realtime").on(
+            'postgres_changes',
+            {
+            event: 'UPDATE',
+            schema: 'public',
+            table: 'Order',
+            filter: 'id=eq.' + order.id
+            },
+            (payload) => {
+              if (!payload.errors) {
+                setCurrent((previous : OrderData) => ({
+                  ...previous,
+                  ...payload.new
+                }))
+              } else {
+                console.log(payload.errors)
+                console.log(payload.new)
+              }
+            }
+        ).subscribe()
+
+        return() => {
+          supabase.removeChannel(channel)
+        }
+    },[])
+
+    return (
+        <OrderDetailsLayout order={current} />
+    )
+}
